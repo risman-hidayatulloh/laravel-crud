ffe406938ad4dc03c4ce4c301845aa861b7d452a
Risman Hidayatulloh <risman100@gmail.com>
2022-11-23 15:46:00 +0700

    deploy

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,9 @@ use App\Http\Controllers\ProductController;
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+Route::get('/', function () {
+    return view('welcome');
+});
+ 
+
 Route::resource('products', ProductController::class);
